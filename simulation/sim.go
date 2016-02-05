b66fc0890f2bd78b8a28ad82468a0540d35cae49
vtphan <vphan@memphis.edu>
2016-02-05 14:55:20 -0600

    .

--- simulation/sim.go
+++ simulation/sim.go
@@ -12,23 +12,23 @@ import (
 	OUTPUT[i] is a random int between 0 and N-1
 	sum of all OUTPUT[i] = population
 */
-func Partition(population int, N int) []int {
+func Partition(population int, cakes int, N int) []int {
 	rand.Seed(time.Now().UTC().UnixNano())
 	perm := rand.Perm(population)
 	pop := make([]int, population)
 	for i := 0; i < len(perm); i++ {
 		pop[perm[i]] = rand.Intn(N)
-		if pop[perm[i]] >= population {
-			pop[perm[i]] = population
+		if pop[perm[i]] >= cakes {
+			pop[perm[i]] = cakes
 			break
 		}
-		population -= pop[perm[i]]
+		cakes -= pop[perm[i]]
 	}
 	return pop
 }
 
 func main() {
-	pop := Partition(50, 11)
+	pop := Partition(50, 100, 20)
 	sum := 0
 	for i := 0; i < len(pop); i++ {
 		if pop[i] > 0 {
